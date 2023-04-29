4aece059cc114911d28672ca4d5a277d59ef610c
karlos1000 <karlos.0@live.com.mx>
2023-04-29 17:13:04 -0600

    Change BrowserRouter by HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import TaskList from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
 // import TaskFormEdit from "./components/TaskFormEdit";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
     return (
         // bg-zinc-600
         <div className="h-screen">
             <div className="container mx-auto">
-                <BrowserRouter>
+                {/* <BrowserRouter> */}
+                <HashRouter>
                     <Routes>
                         <Route path="/" element={<TaskList />} />
                         <Route path="/create-task" element={<TaskForm />} />
                         <Route path="/edit-task/:id" element={<TaskForm />} />
                         {/* <Route path="/edit-task/:id" element={<TaskFormEdit />} /> */}
                     </Routes>
-                </BrowserRouter>
+                </HashRouter>
+                {/* </BrowserRouter> */}
             </div>
         </div>
     );
